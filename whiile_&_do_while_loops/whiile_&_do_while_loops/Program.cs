3fa9cae93237e07a6d4442e94105658042ed1640
Takeshwar Jangita <takeshwarjangita@gmail.com>
2026-01-07 14:55:34 +0530

    Add files via upload

---
+++ whiile_&_do_while_loops/whiile_&_do_while_loops/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+public class WhileLoops
+{
+    public static void Main(string[] args)
+    {
+        /*nt i = 0;
+         while(i<10)
+         {
+             Console.WriteLine(i);
+             i++;
+         }*/
+        /*
+        int i = 1;
+        while (i < 10)
+        {
+            i++;
+            Console.WriteLine(i);
+        }
+
+        */
+        /*int i = 0;
+        do
+        {
+            
+            Console.WriteLine(i);
+            i++;
+
+        } while (i< 10);*/
+        /*Console.WriteLine("enter your name");
+        string n = Console.ReadLine();
+        */
+        //while
+
+        /*while (n!="takesh")
+        {
+            Console.WriteLine("enter correct user name");
+            n= Console.ReadLine();
+         
+                
+        }
+        Console.WriteLine("correct");*/
+
+
+
+        //do-while
+        string n;
+        do
+        {
+            Console.WriteLine("enter your name");
+            n=Console.ReadLine();
+            if (n == "takesh")
+            {
+                Console.WriteLine("correct");
+            }
+            else
+            {
+                Console.WriteLine("incorrect");
+
+            }
+        } while (n != "takesh");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Console.ReadLine();
+    }
+}
