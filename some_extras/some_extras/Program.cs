a7501050c92e26f820c5480f0d5332ab5e0caba3
Takeshwar Jangita <takeshwarjangita@gmail.com>
2026-01-07 15:18:07 +0530

    Add files via upload

---
+++ some_extras/some_extras/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class extras
+{
+    public static void Main(string[] args)
+    {
+
+        //ternary operator
+        Console.WriteLine("enter age below 19");
+        int a = Convert.ToInt32(Console.ReadLine());
+        //string result = a > 12  ? "teen" : "kid";
+        //Console.WriteLine("result");
+        //Console.WriteLine(a > 12 ? "teen" : "kid");
+
+        //nested ternary
+        string group = a < 19 ? "teen" :
+            a < 13 ? "kid" :
+            "adult";
+        Console.WriteLine(group);
+    }
+}
