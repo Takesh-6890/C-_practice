de7d6e4fa83101793fe5aeebd1a3be30db42daf6
Takeshwar Jangita <takeshwarjangita@gmail.com>
2026-01-09 09:32:18 +0530

    Add files via upload

---
+++ NUM_PATERN_1/NUM_PATERN_1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+public class num_pattern
+{
+    public static void Main()
+    {
+        //number triangle pattern
+        int n = 5;
+        for(int i=1;i<=n;i++)
+        {
+            for (int j= 1;j <= i;j++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+
+        //Floyd's Triangle
+        int num= 1;
+        for (int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                Console.Write(num + " ");
+                num++;
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+
+
+        //inverted number triangle
+        for(int i = n; i >= 0; i--)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        //next number triangle
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+        //inverted next number triangle;
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                Console.Write(j + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
