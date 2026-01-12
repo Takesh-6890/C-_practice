1be104b0348a4f0d06a1276f938628a048911aba
Takeshwar Jangita <takeshwarjangita@gmail.com>
2026-01-12 16:32:52 +0530

    Add files via upload

---
+++ Method_overloadig/Method_overloadig/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+//method overloadind using params
+class Mathops
+{
+    public int Add(params int[] numbers)
+    {
+        int sum = 0;
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+        return sum;
+    }
+
+    static void Main()
+    {
+        Mathops m = new Mathops();
+        Console.WriteLine(m.Add(1, 3, 4));
+    }
+}
---
+++ method_overriding/method_overriding/Program.cs
@@ -0,0 +1,29 @@
+﻿// method overriding 
+using System;
+class Home
+{
+    public virtual void People()
+    {
+        Console.WriteLine("people live here");
+    }
+
+}
+class Boy : Home
+{
+    public override void People() //we can user public sealed override void People(); , to stop the grand child heritance
+    {
+        Console.WriteLine("but i'm the creature");
+    }
+    
+}
+class Program
+{
+    static void Main()
+    {
+        Home a = new Boy();
+        a.People();
+    }
+}
+
+
+
