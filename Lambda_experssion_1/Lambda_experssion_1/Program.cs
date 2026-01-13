1bb18b358ae5dcffd24d1504fb71f4df8eaf0983
Takeshwar Jangita <takeshwarjangita@gmail.com>
2026-01-13 07:31:58 +0530

    Add files via upload

---
+++ Lambda_experssion_1/Lambda_experssion_1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*lambda experssion
+(a, b) => (a + b);
+
+lambds with sinlge parameter
+x=>x*x;
+no paramerts
+()=>Console.WriteLine("hello");
+
+mltiple statements
+(x,y)=>
+{
+int sum=x+y;
+retutrn suml
+}
+
+*/
+
+
+//func<>
+using System;
+class Program
+{
+    static void Main()
+    {
+        Func<int, int ,int> sum = (x, y) => x + y;
+        Console.WriteLine(sum(20,5));
+
+        Action<string>greet=person=> Console.WriteLine((("welcome" + person)));
+        greet("takesh");
+
+    }
+}
+
+
