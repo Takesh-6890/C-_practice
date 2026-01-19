7b460223f8c2710290c315c073a076cb855c4ab1
Takeshwar Jangita <takeshwarjangita@gmail.com>
2026-01-19 10:55:35 +0530

    Add files via upload

---
+++ OOPS_1/Bank-AC_Back_end_EncapSulation/Bank-AC_Back_end_EncapSulation/Back_end.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank_AC_Back_end_EncapSulation
+{
+     class BankAC
+    {
+        private decimal balance;
+        public decimal Balance
+        {
+            get => balance;
+            private set => balance = value;
+        }
+        public void Deposite(decimal amount)
+        {
+            if(amount<=0)
+            {
+                throw new Exception("invaild deposite");
+            }
+            Balance += amount;
+
+        }
+
+        public void Withdrawl(decimal amount)
+        {
+            if(amount >balance)
+            {
+                throw new Exception("insufficiant balance");
+            }
+            
+            Balance -=amount;
+        }
+    }
+}
---
+++ OOPS_1/Bank-AC_Back_end_EncapSulation/Bank-AC_Back_end_EncapSulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using Bank_AC_Back_end_EncapSulation;
+using System;
+class program
+{
+    static void Main()
+    {
+        BankAC acc = new BankAC();
+        acc.Deposite(100);
+        acc.Withdrawl(50);
+        Console.WriteLine($"balance {acc.Balance}");
+    }
+}
