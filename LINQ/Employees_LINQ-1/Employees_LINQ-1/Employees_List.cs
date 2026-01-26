232e487d9224a3206066501bb564b0a042b52ce4
Takeshwar Jangita <takeshwarjangita@gmail.com>
2026-01-26 09:11:47 +0530

    Add files via upload

---
+++ LINQ/Employees_LINQ-1/Employees_LINQ-1/Employees_List.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Employees_LINQ_1;
+
+
+namespace Employees_LINQ_1
+{
+    class Employees
+    {
+        public int id{ get; set; }
+        public string name { get; set; }
+        public int salary { get; set; }
+    }
+}
