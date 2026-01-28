baac77488a07075f2e717255b4c8dac86c5061b4
Takeshwar Jangita <takeshwarjangita@gmail.com>
2026-01-28 08:46:47 +0530

    Add files via upload

---
+++ Order_Preprocessing_Delegates/Order_Preprocessing_Delegates/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Order
+{
+    public int Quantity { get; set; }
+    public int Price { get; set; }
+}
+class Program
+{
+    static void Main()
+    {
+        Predicate<Order> IsvalidOrder=Order=>Order.Quantity >0 && Order.Price >0;
+
+        Func<Order,double> CalTotal=Order=>Order.Quantity*Order.Price;
+
+        Action<string> notify = msg => Console.WriteLine("notification:" + msg);
+
+        Console.WriteLine("enter the quantity of the porduct");
+        int quantity=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter the price of the each product");
+        int price=Convert.ToInt32(Console.ReadLine());  
+
+        Order Myorder = new Order();
+        {
+            Myorder.Quantity = quantity;
+            Myorder.Price = price;
+           
+            
+
+        }
+        if(IsvalidOrder(Myorder))
+        {
+            double total = CalTotal(Myorder);
+            notify($"order placed , total amount {total}");
+        }
+        else
+        {
+            notify("invalid order");
+        }
+    }
+}
